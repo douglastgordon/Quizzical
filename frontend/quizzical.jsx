00e4f53ab4462cd01650e35bd8020dc615d94e4c
Douglas Gordon <DouglasTGordon@Douglas.local>
2016-10-04 11:54:57 -0400

    signup, login, logout working

--- frontend/quizzical.jsx
+++ frontend/quizzical.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import { login, logout, signup } from './actions/session_actions.js';
+
+window.login = login;
+window.logout = logout;
+window.signup = signup;
+
 document.addEventListener('DOMContentLoaded', () => {
+    window.store = configureStore();
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Quizzical</h1>, root);
 });
