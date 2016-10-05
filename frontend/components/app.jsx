e05db7bb4abe9002bce5e2b69390b191f19e1f66
Douglas Gordon <DouglasTGordon@Douglas.local>
2016-10-05 11:41:32 -0400

    add tree

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
 import GreetingContainer from './greeting/greeting_container';
+import LoggedOut from './logged_out/logged_out';
 
 const App = ({children}) => (
-  <div>
+  <div className="group">
     <header className="navBar group">
       <Link to="/" className="header-link"><h1>Quizzical</h1></Link>
       <GreetingContainer/>
     </header>
+    <LoggedOut/>
     {children}
   </div>
 );
---
+++ frontend/components/logged_out/logged_out.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class loggedout extends React.Component{
+  render(){
+    return (
+    <div>
+      <img src={window.Quizzical.images.learningtree} className="tree"></img>
+      <h2>Learn Anything.</h2>
+      <p>Lorem impsum dolor sit amet.</p>
+    </div>
+  );
+  }
+}
