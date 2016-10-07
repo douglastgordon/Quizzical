dc248c99973a1dc97c15781d1fd247dd4c30ac91
Douglas Gordon <DouglasTGordon@Douglas.local>
2016-10-07 10:16:08 -0400

    cards controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,5 +1,27 @@
 class Api::CardsController < ApplicationController
 
+  def show
+    @card = Card.find(params[:id])
+    render :show
+  end
+
+  def create
+    @card = Card.create(card_params)
+    render :show
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    render :index
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update!(card_params)
+    render :show
+  end
+
   private
 
   def card_params
