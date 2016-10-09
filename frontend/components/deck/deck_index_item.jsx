d407d7a41f9981d39dc51abf0fd60cebd6c70ddc
DouglasTGordon <douglastgordon@gmail.com>
2016-10-09 17:05:34 -0400

    added languages to index page

--- frontend/components/deck/deck_index_item.jsx
+++ frontend/components/deck/deck_index_item.jsx
@@ -17,7 +17,10 @@ export default class DeckIndexItem extends React.Component{
   render(){
     return (
       <li>
-        <h2 onClick={this.handleClick}>{this.props.deck.title}</h2>
+        <div className="top-line group">
+          <h2 onClick={this.handleClick}>{this.props.deck.title}</h2>
+          <h4>{this.props.deck.term_language+" > "+this.props.deck.definition_language}</h4>
+        </div>
         <Link to={"/users/"+this.props.deck.author_id}><h3>{this.props.deck.author}</h3></Link>
       </li>
     );
