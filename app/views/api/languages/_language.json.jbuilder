37e1734e40b0c550f26e2b706ea21edf68a421df
DouglasTGordon <douglastgordon@gmail.com>
2016-10-11 10:23:33 -0400

    style languages page

--- app/views/api/languages/_language.json.jbuilder
+++ app/views/api/languages/_language.json.jbuilder
@@ -1,1 +1,2 @@
 json.extract! language, :id, :name
+json.def_lang_count language.to_decks.count
--- frontend/components/language/language_index.jsx
+++ frontend/components/language/language_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default class LanguageIndex extends React.Component {
 
   constructor(props){
@@ -9,7 +10,18 @@ export default class LanguageIndex extends React.Component {
   render(){
     let languageKeys = Object.keys(this.props.languages);
     let languages = languageKeys.map((key) => {
-      return (<li>{this.props.languages[key].name}</li>);
+      return (
+        <li>
+          <div className="top-line">
+            <h2>{this.props.languages[key].name}</h2>
+          </div>
+          <div className="right-side">
+            <div className="deck-count">
+              <h5>{this.props.languages[key].def_lang_count + " decks"}</h5>
+            </div>
+            <img src={window.Quizzical.images.enter} className="enter"></img>
+          </div>
+        </li>);
     });
 
 
