90554311ec27a58a38e5d1bda0ff9984d25bfc1d
DouglasTGordon <douglastgordon@gmail.com>
2016-10-26 10:53:47 -0400

    fix guest login

--- frontend/components/session_forms/session_form_component.jsx
+++ frontend/components/session_forms/session_form_component.jsx
@@ -42,7 +42,7 @@ export default class SessionFormComponent extends React.Component {
     e.preventDefault();
     hashHistory.push("/login");
     this.setState({username: "Guest", password: "starwars"});
-    this.handleSubmit(e);
+    this.processForm({username: "Guest", password: "starwars"});
   }
 
 
