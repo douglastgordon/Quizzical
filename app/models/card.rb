77769c449acefe24a6d4be7e7a1bd3bc410531e3
DouglasTGordon <douglastgordon@gmail.com>
2016-10-28 17:56:10 -0400

    remove default

--- app/models/card.rb
+++ app/models/card.rb
@@ -16,7 +16,7 @@ class Card < ActiveRecord::Base
 
   validates :term, :definition, :deck_id, presence: true
 
-  has_attached_file :audio,  default_url: "/assets/audio/sample.wav"
+  has_attached_file :audio
   validates_attachment_content_type :audio, content_type: /\Aaudio\/.*\z/
 
   belongs_to :deck
